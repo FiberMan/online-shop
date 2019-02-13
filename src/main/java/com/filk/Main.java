ed3415fc1d811dd32a2337799d1eb8d786e66a60
Yurii Filimonchuk <yfilimonchuk@luxoft.com>
2019-02-13 22:54:47 +0200

    Initial commit

---
+++ src/main/java/com/filk/Main.java
@@ -0,0 +1,13 @@
+package com.filk;
+
+public class Main {
+
+    // TODO: prepareStatement
+    // TODO: data, mapper, service, presentation layers
+    // TODO: bootstrap
+    // TODO: login
+    // TODO: add servlet filters
+    // TODO: connection through DataSource
+    // TODO: store login/pass -> hash
+    // TODO: read: Optional, DispatcherType
+}
