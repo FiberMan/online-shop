6c5ec07797885a417226e28f860aa800ec230183
Yuriy Filimonchuk <filimonchuk@gmail.com>
2019-02-20 03:12:14 +0200

    Fix template path.

--- src/main/java/com/filk/web/utils/PageGenerator.java
+++ src/main/java/com/filk/web/utils/PageGenerator.java
@@ -11,7 +11,7 @@ import java.io.Writer;
 import java.util.Map;
 
 public class PageGenerator {
-    private static final String HTML_DIR = "src\\main\\resources\\templates";
+    private static final String HTML_DIR = "src/main/resources/templates";
 
     private static PageGenerator pageGenerator;
     private final Configuration cfg;
